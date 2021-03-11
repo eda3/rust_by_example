8c1d0800772f04f2ed3f76b931e931d9efc1d6b2
eda3 <gerorin1010@gmail.com>
2021-03-12 06:30:07 +0900

    add: 固定長の配列

--- src/bin/2-2-3-0_array.rs
+++ src/bin/2-2-3-0_array.rs
@@ -1,10 +1,19 @@
 use std::mem;
 
-fn analyze_slice(slice: &[i32]){
+fn analyze_slice(slice: &[i32]) {
   println!("sliceの第一要素: {}", slice[0]);
   println!("sliceは{}個の要素を持つ", slice.len());
 }
 
 fn main() {
+  // 固定長の配列
+  let xs: [i32; 5] = [1, 2, 3, 4, 5];
+  // すべての要素を0にする場合
+  let ys: [i32; 500] = [0; 500];
 
+  // インデックスは0からスタート
+  println!("配列の1つ目の要素: {}", xs[0]);
+  println!("配列の2つ目の要素: {}", xs[1]);
+  // -> 配列の1つ目の要素: 1
+  // -> 配列の2つ目の要素: 2
 }
