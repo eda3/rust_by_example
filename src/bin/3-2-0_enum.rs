a970f17b5b89e9e975c651d267213fe2950b5f86
eda3 <gerorin1010@gmail.com>
2021-03-14 20:05:57 +0900

    型エイリアス

--- src/bin/3-2-0_enum.rs
+++ src/bin/3-2-0_enum.rs
@@ -22,4 +22,44 @@ fn inspect(event: WebEvent) {
   }
 }
 
-fn main() {}
+// 型エイリアス
+// 型エイリアスを使用するとenumの型名を別名で参照することができる
+enum VeriVerboseEnumOfThingsToDoWithNumbers {
+  Add,
+  Subtract,
+}
+
+// 型エイリアスの作成
+type Operations = VeriVerboseEnumOfThingsToDoWithNumbers;
+
+impl Operations {
+  fn run(&self, x: i32, y: i32) -> i32 {
+    match self {
+      Self::Add => x + y,
+      Self::Subtract => x - y,
+    }
+  }
+}
+
+fn main() {
+  let pressed = WebEvent::KeyPress('x');
+  // to_owned()は&strをStringに変換する
+  let pasted = WebEvent::Paste("my text".to_owned());
+  let click = WebEvent::Click { x: 20, y: 80 };
+  let load = WebEvent::PageLoad;
+  let unload = WebEvent::PageUnload;
+
+  inspect(pressed);
+  inspect(pasted);
+  inspect(click);
+  inspect(load);
+  inspect(unload);
+  // -> pressed x
+  // -> pasted my text
+  // -> clicked at x=20, y=80
+  // -> page loaded
+  // -> page unloaded
+
+  // 型エイリアス
+  let x = Operations::Add;
+}
