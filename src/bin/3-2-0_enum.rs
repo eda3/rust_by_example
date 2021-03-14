86a736720cda1ec6268214683f8f65c4ddc7d07a
eda3 <gerorin1010@gmail.com>
2021-03-14 19:55:46 +0900

    add: inspect関数

--- src/bin/3-2-0_enum.rs
+++ src/bin/3-2-0_enum.rs
@@ -8,4 +8,18 @@ enum WebEvent {
   // C言語構造体ライク
   Click { x: i64, y: i64 },
 }
+
+// WebEvent列挙型を引数にとり、何も返却しない関数
+fn inspect(event: WebEvent) {
+  match event {
+    WebEvent::PageLoad => println!("page loaded"),
+    WebEvent::PageUnload => println!("page unloaded"),
+    WebEvent::KeyPress(c) => println!("pressed {}", c),
+    WebEvent::Paste(s) => println!("pasted {}", s),
+    WebEvent::Click { x, y} => {
+      println!("clicked at x={}, y={}", x, y);
+    },
+  }
+}
+
 fn main() {}
