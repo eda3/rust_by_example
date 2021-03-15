7bd6d892298a67deac48e75b4cb71c17f6e23e78
eda3 <gerorin1010@gmail.com>
2021-03-16 06:42:34 +0900

    変数名の頭に_をつけることでコンパイル時の警告をなくすことができる

--- src/bin/4-0_variable_bindings.rs
+++ src/bin/4-0_variable_bindings.rs
@@ -11,4 +11,17 @@ fn main() {
   // -> an_integer: 1
   // -> a_boolean: true
   // -> unit: ()
+
+  // 使用されていない変数があるとコンパイラは警告を出します
+  // 変数名の頭に`_`(アンダーバー)をつけると警告を消すことができます
+  let _unused_variable = 3u32;
+
+  let noisy_unused_variable = 2u32;
+  // 以下の警告が出力される
+  // --> src\bin\4-0_variable_bindings.rs:19:7
+  //      |
+  //   19 |   let noisy_unused_variable = 2u32;
+  //      |       ^^^^^^^^^^^^^^^^^^^^^ help: if this is intentional, prefix it with an underscore: `_noisy_unused_variable`
+  //      |
+  // = note: `#[warn(unused_variables)]` on by default
 }
