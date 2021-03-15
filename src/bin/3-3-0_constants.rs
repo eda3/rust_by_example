3101fa114b0a2cb8393e39cb2da1dffa3ac06b03
eda3 <gerorin1010@gmail.com>
2021-03-16 06:26:58 +0900

    add: Verification of static,const

--- src/bin/3-3-0_constants.rs
+++ src/bin/3-3-0_constants.rs
@@ -5,4 +5,21 @@ fn is_big(n: i32) -> bool {
   // 関数内から定数を参照
   n > THRESHOLD
 }
-fn main() {}
+fn main() {
+  let n = 16;
+  // main 関数の中から定数を参照
+  println!("これは{}", LANGUAGE);
+  println!("しきい値は{}", THRESHOLD);
+  println!("{}は{}", n, if is_big(n) { "大きい" } else { "小さい" });
+  // -> これは Rust
+  // -> しきい値は10
+  // -> 16は大きい
+
+  // コンパイルエラー
+  // THRESHOLD = 5;
+  //
+  // 18 |   THRESHOLD = 5;
+  //    |   --------- ^
+  //    |   |
+  //    |   cannot assign to this expression
+}
