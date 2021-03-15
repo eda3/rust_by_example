61ed2fddb436f067a54a850a38b8d33f8c650572
eda3 <gerorin1010@gmail.com>
2021-03-16 06:38:10 +0900

    add: コンソールへ出力

--- src/bin/4-0_variable_bindings.rs
+++ src/bin/4-0_variable_bindings.rs
@@ -2,4 +2,13 @@ fn main() {
   let an_integer = 1u32;
   let a_boolean = true;
   let unit = ();
+
+  // an_integerくぉcopied_integerへとコピー
+  let copied_integer = an_integer;
+  println!("an_integer: {:?}", copied_integer);
+  println!("a_boolean: {:?}", a_boolean);
+  println!("unit: {:?}", unit);
+  // -> an_integer: 1
+  // -> a_boolean: true
+  // -> unit: ()
 }
