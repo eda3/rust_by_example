0762eaf1e08dbad42aab046acbe38237ffa1d340
eda3 <gerorin1010@gmail.com>
2021-03-15 20:11:32 +0900

    add: len関数追加

--- src/bin/3-2-3-0_testcate_linked_list.rs
+++ src/bin/3-2-3-0_testcate_linked_list.rs
@@ -1,5 +1,6 @@
 use crate::List::*;
 
+#[derive(Debug)]
 enum List {
   // 要素をラップして、次の要素へのポインタを保持するタプル
   Cons(u32, Box<List>),
@@ -14,10 +15,23 @@ impl List {
     Nil
   }
 
-  // リストを受け取り、その始端に新しい要素を付加したものを返す関数
+  // リストを受け取り、その始端に新しい要素を付加したものを返すメソッド
   fn prepend(self, elem: u32) -> List {
     // この`Cons`自体も、その第二要素もどちらもList型
     Cons(elem, Box::new(self))
+  }
+
+  // list の長さを返すメソッド
+  fn len(&self) -> u32 {
+    // このメソッドはselfの状態に寄って振る舞いが変化するためmatchする必要がある
+    // selfの型は&Listであるので、*selfはListになる。マッチングは
+    // 参照(&T)ではなく実体(T)に対して行うのが好ましい
+    match *self {
+      // selfをすでに借用しているので、tailの所有権を取ることができない
+      // 代わりに参照を使用する
+      Cons(_, ref tail) => 1 + tail.len(),
+      Nil => 0,
+    }
   }
 }
 
@@ -29,4 +43,9 @@ fn main() {
   list = list.prepend(1);
   list = list.prepend(2);
   list = list.prepend(3);
+  println!("{:?}", list);
+  // -> Cons(3, Cons(2, Cons(1, Nil)))
+
+  // 追加後の状態を表示
+  println!("list.len: {}", list.len());
 }
