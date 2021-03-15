7a4ad9437afb9cc70d06240459f3d0cecc2ad6c6
eda3 <gerorin1010@gmail.com>
2021-03-15 18:53:03 +0900

    use Status, Civilian

--- src/bin/3-2-1-0_enum_use.rs
+++ src/bin/3-2-1-0_enum_use.rs
@@ -10,4 +10,14 @@ enum Work {
   Soldier,
 }
 
-fn main() {}
+fn main() {
+  // `use` することで絶対名でなくとも使用可能になる。
+  use crate::Status::{Poor, Rich};
+  // `Work` の中の名前を全て`use`する
+  use crate::Work::*;
+
+  // `use` しているため、`Status::Poor`と同じ
+  let status = Poor;
+  // `Work::Civilian`と等しい
+  let work = Civilian;
+}
