1befaa0fa250a8ee850a1887d117f338c53e4ae8
eda3 <gerorin1010@gmail.com>
2021-03-15 19:50:55 +0900

    add: prepend関数

--- src/bin/3-2-3-0_testcate_linked_list.rs
+++ src/bin/3-2-3-0_testcate_linked_list.rs
@@ -13,6 +13,20 @@ impl List {
     // `Nil` は `List`型を持つ
     Nil
   }
+
+  // リストを受け取り、その始端に新しい要素を付加したものを返す関数
+  fn prepend(self, elem: u32) -> List {
+    // この`Cons`自体も、その第二要素もどちらもList型
+    Cons(elem, Box::new(self))
+  }
 }
 
-fn main() {}
+fn main() {
+  // 空の連結リストを作成
+  let mut list = List::new();
+
+  // 要素を追加
+  list = list.prepend(1);
+  list = list.prepend(2);
+  list = list.prepend(3);
+}
