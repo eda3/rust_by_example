bfde7dae9c5e30756d13e8c915ad90092f21b6d0
eda3 <gerorin1010@gmail.com>
2021-03-15 19:07:53 +0900

    add: match status, work

--- src/bin/3-2-1-0_enum_use.rs
+++ src/bin/3-2-1-0_enum_use.rs
@@ -20,4 +20,17 @@ fn main() {
   let status = Poor;
   // `Work::Civilian`と等しい
   let work = Civilian;
+
+  match status {
+    // `use`しているのでスコープを明示していない
+    Rich => println!("お金持ちはたくさんお金を持っている"),
+    Poor => println!("貧乏人はお金を持っていない"),
+  }
+  // -> 貧乏人はお金を持っていない
+
+  match work {
+    Civilian => println!("市民は働く！"),
+    Soldier => println!("兵士は戦う！"),
+  }
+  // -> 市民は働く！
 }
