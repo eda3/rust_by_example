0cc62abf045d7f51400e864d07752526725e9a21
eda3 <gerorin1010@gmail.com>
2021-03-18 06:33:07 +0900

    std::mem::size_of_val()で各変数のバイト数を表示

--- src/bin/5-2-0_literals.rs
+++ src/bin/5-2-0_literals.rs
@@ -7,4 +7,17 @@ fn main() {
   // サフィックスを指定しないリテラル。型は使用方法に依存
   let i = 1;
   let f = 1.0;
+
+  // std::mem::size_of_val : 変数のサイズをバイトで返す
+  println!("x(1u8)のバイト数: {}", std::mem::size_of_val(&x));
+  println!("y(2u32)のバイト数: {}", std::mem::size_of_val(&y));
+  println!("z(3f32)のバイト数: {}", std::mem::size_of_val(&z));
+  println!("i(i32)のバイト数: {}", std::mem::size_of_val(&i));
+  println!("f(f64)のバイト数: {}", std::mem::size_of_val(&f));
+  // -> x(1u8)のバイト数: 1
+  // -> y(2u32)のバイト数: 4
+  // -> z(3f32)のバイト数: 4
+  // -> i(i32)のバイト数: 4
+  // -> f(f64)のバイト数: 8
+
 }
