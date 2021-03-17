060a61b3a51a1e4beb18ce5673a390f32f3ce9c1
eda3 <gerorin1010@gmail.com>
2021-03-18 04:21:59 +0900

    _mutable_integerをイミュータブルにシャドーイング

--- src/bin/4-4-0_freeze.rs
+++ src/bin/4-4-0_freeze.rs
@@ -1,1 +1,13 @@
-fn main() {}
+fn main() {
+  let mut _mutable_integer = 7i32;
+  {
+    // _mutable_integer をイミュータブルにシャドーイング
+    let _mutable_integer = _mutable_integer;
+
+    // Error! _mutable_integerはこのスコープではフリーズしている
+    // _mutable_integer = 50;
+    //
+    // |     _mutable_integer = 50;
+    // |     ^^^^^^^^^^^^^^^^^^^^^ cannot assign twice to immutable variable
+  }
+}
