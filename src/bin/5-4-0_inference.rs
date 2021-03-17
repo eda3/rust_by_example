6410f86ebf85c7fb5a9dda681a73ab0db4dc8069
eda3 <gerorin1010@gmail.com>
2021-03-18 06:47:13 +0900

    アノテーションで型が決定される例

--- src/bin/5-4-0_inference.rs
+++ src/bin/5-4-0_inference.rs
@@ -1,1 +1,10 @@
-fn main() {}
+fn type_of<T>(_: T) -> String {
+  let a = std::any::type_name::<T>();
+  a.to_string()
+}
+fn main() {
+  // アノテーションのおかげで、コンパイラはelemがu8型であることがわかる
+  let elem = 5u8;
+  println!("{}", type_of(elem));
+  // -> u8
+}
