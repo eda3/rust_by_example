f7e31766a7187df336fa720eaeb61a07ce832bc5
eda3 <gerorin1010@gmail.com>
2021-03-18 06:55:51 +0900

    Vecを使って型推論の確認

--- src/bin/5-4-0_inference.rs
+++ src/bin/5-4-0_inference.rs
@@ -7,4 +7,15 @@ fn main() {
   let elem = 5u8;
   println!("{}", type_of(elem));
   // -> u8
+
+  // 空のベクトル(可変長の配列)を生成
+  let mut vec = Vec::new();
+
+  // この時点でvecの型がVec<u8>だと型推論される
+  vec.push(elem);
+
+  println!("{:?}", vec);
+  // -> [5]
+  println!("{}", type_of(vec));
+  // -> alloc::vec::Vec<u8>
 }
