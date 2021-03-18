5091902302043bdfc2ad982c07d447f05e17dad2
eda3 <gerorin1010@gmail.com>
2021-03-19 05:32:33 +0900

    typeを使い型にエイリアスをつける

--- src/bin/5-4-0_alias.rs
+++ src/bin/5-4-0_alias.rs
@@ -2,6 +2,22 @@
 type NanoSecond = u64;
 type Inch = u64;
 
-// キャメルケース時の警告を抑える
+// キャメルケースじゃない時の警告を抑える
 #[allow(non_camel_case_types)]
-fn main() {}
+type u64_t = u64;
+// ^^^^^ help: convert the identifier to upper camel case: `U64T`
+// #[allow(non_camel_case_types)]がないとき、↑の警告が起こる
+
+fn main() {
+  let nanoseconds: NanoSecond = 5 as u64_t;
+  let inches: Inch = 2 as u64_t;
+
+  // 型のエイリアスは元の型をより型安全にしれくれる **わけではない ** ことに注意しましょう
+  println!(
+    "{} nanoseconds + {} inches = {} unit?",
+    nanoseconds,
+    inches,
+    nanoseconds + inches
+  );
+  // -> 5 nanoseconds + 2 inches = 7 unit?
+}
