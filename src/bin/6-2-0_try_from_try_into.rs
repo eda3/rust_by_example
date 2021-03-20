f9ff1256fade62148aac42b1f025c31aee52e0a8
eda3 <gerorin1010@gmail.com>
2021-03-20 12:41:36 +0900

    説明文の追加

--- src/bin/6-2-0_try_from_try_into.rs
+++ src/bin/6-2-0_try_from_try_into.rs
@@ -1,1 +1,7 @@
+// Similar to From and Into, TryFrom and TryInto are generic traits for converting between types.
+// FromやIntoと同様に、TryFromやTryIntoは型間の変換を行うための汎用的なcrate
+
+// Unlike From/Into, the TryFrom/TryInto traits are used for fallible conversions, and as such, return Results.
+// From/Intoとは異なり、TryFrom/TryInto trateは誤りやすい変換に使用され、そのような場合はResultを返す
+
 fn main() {}
