ac0a93e942259d759b3b61210a75010a1ddaed07
eda3 <gerorin1010@gmail.com>
2021-03-20 12:45:09 +0900

    add: struct EvenNumber

--- src/bin/6-2-0_try_from_try_into.rs
+++ src/bin/6-2-0_try_from_try_into.rs
@@ -4,4 +4,10 @@
 // Unlike From/Into, the TryFrom/TryInto traits are used for fallible conversions, and as such, return Results.
 // From/Intoとは異なり、TryFrom/TryInto trateは誤りやすい変換に使用され、そのような場合はResultを返す
 
+use std::convert::TryFrom;
+use std::convert::TryInto;
+
+#[derive(Debug, Pa)]
+struct EvenNumber(i32);
+
 fn main() {}
