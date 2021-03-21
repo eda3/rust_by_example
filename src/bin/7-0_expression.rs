eb16ea1a796cc9f9426965054b0863fdecdd5989
eda3 <gerorin1010@gmail.com>
2021-03-21 11:14:59 +0900

    コードブロック式

--- src/bin/7-0_expression.rs
+++ src/bin/7-0_expression.rs
@@ -6,4 +6,26 @@ fn main() {
   x;
   x + 1;
   15;
+
+  let x = 5u32;
+
+  let y = {
+    let x_squared = x * x;
+    let x_cube = x_squared * x;
+
+    // この式はyに代入されます
+    x_cube + x_squared + x
+  };
+
+  let z = {
+    // セミコロンがあるのでzには()が入ります
+    2 * x;
+  };
+
+  println!("x is {:?}", x);
+  // x is 5
+  println!("y is {:?}", y);
+  // y is 155
+  println!("z is {:?}", z);
+  // z is ()
 }
