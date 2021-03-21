258a0250f974e2cb4e722a095a93310a7fcc49fa
eda3 <gerorin1010@gmail.com>
2021-03-21 09:45:20 +0900

    解説追加

--- src/bin/6-3-0_string.rs
+++ src/bin/6-3-0_string.rs
@@ -1,1 +1,6 @@
+// To convert any type to a String is as simple as implementing the ToString trait for the type.
+// 任意の型を String に変換するには、その型の ToString traitを実装するだけです。
+// Rather than doing so directly, you should implement the fmt::Display trait which automagically provides ToString and also allows printing the type as discussed in the section on print!.
+// 直接行うのではなく、自動的に ToString を提供する fmt::Display trait を実装するべきであり、また、 print のセクションで説明したように、型をprintすることもできます!
+
 fn main() {}
