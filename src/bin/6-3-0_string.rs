9081bc54393fe3636d446b0370fbe69b6e193adf
eda3 <gerorin1010@gmail.com>
2021-03-21 09:46:58 +0900

    add: struct Circle

--- src/bin/6-3-0_string.rs
+++ src/bin/6-3-0_string.rs
@@ -3,4 +3,10 @@
 // Rather than doing so directly, you should implement the fmt::Display trait which automagically provides ToString and also allows printing the type as discussed in the section on print!.
 // 直接行うのではなく、自動的に ToString を提供する fmt::Display trait を実装するべきであり、また、 print のセクションで説明したように、型をprintすることもできます!
 
+use std::fmt;
+
+struct Circle {
+  radius: i32,
+}
+
 fn main() {}
