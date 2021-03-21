fbfd017180675e8f00ebe45556f357b4c42bb746
eda3 <gerorin1010@gmail.com>
2021-03-21 15:16:44 +0900

    借用を行ったクロージャをコール

--- src/bin/9-2-1_capture.rs
+++ src/bin/9-2-1_capture.rs
@@ -1,1 +1,13 @@
-fn main() {}
+fn main() {
+  use std::mem;
+  let color = "green";
+
+  // 変数colorをprintするためのクロージャ
+  // これはcolorを借用(&)し、その借用とクロージャをprintという名の変数に保持する
+  // 借用はprintがスコープから出るまで続く
+  let print = || println!("color: {}", color);
+
+  // 借用を行ったクロージャをコールする
+  print();
+  // -> color: green
+}
