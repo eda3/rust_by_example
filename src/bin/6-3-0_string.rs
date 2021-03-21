3153206d1f0dd23ad2b071db7f8cb49cd83f2327
eda3 <gerorin1010@gmail.com>
2021-03-21 10:34:14 +0900

    解説追加

--- src/bin/6-3-0_string.rs
+++ src/bin/6-3-0_string.rs
@@ -1,5 +1,6 @@
 // To convert any type to a String is as simple as implementing the ToString trait for the type.
 // 任意の型を String に変換するには、その型の ToString traitを実装するだけです。
+
 // Rather than doing so directly, you should implement the fmt::Display trait which automagically provides ToString and also allows printing the type as discussed in the section on print!.
 // 直接行うのではなく、自動的に ToString を提供する fmt::Display trait を実装するべきであり、また、 print のセクションで説明したように、型をprintすることもできます!
 
@@ -24,4 +25,27 @@ fn main() {
   // -> Circle { radius: 6 }
   println!("{:?}", circle.to_string())
   // -> "Circleの半径は6"
+
+  // One of the more common types to convert a string into is a number.
+  // 文字列を数値に変換する際によく使われる型のひとつが「数値型」です。
+
+  // The idiomatic approach to this is to use the parse function and either to arrange for type
+  // inference or to specify the type to parse using the 'turbofish' syntax.
+  // この場合の慣用的な方法は、parse関数を使用して型の推論を行うか、「turbofish」構文を使用してparseする型を指定
+  // することです。
+
+  // Both alternatives are shown in the following example.
+  // 次の例では、どちらの方法も使用できます。
+
+  // This will convert the string into the type specified so long as the FromStr trait is
+  // implemented for that type.
+  // これにより、FromStr traitがその型に対して実装されている限り、文字列は指定された型に変換されます。
+
+  // This is implemented for numerous types within the standard library.
+  // これは、標準ライブラリの多くの型に実装されています。
+
+  // To obtain this functionality on a user defined type simply implement the FromStr trait for
+  // that type.
+  // ユーザー定義の型でこの機能を利用するには、その型のFromStr形質を実装するだけです。
+
 }
