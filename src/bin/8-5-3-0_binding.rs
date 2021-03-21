87b515e0017956932ec69649ed84424a532e374d
eda3 <gerorin1010@gmail.com>
2021-03-21 13:11:01 +0900

    @マークを使用して変数をバインディングすることができる

--- src/bin/8-5-3-0_binding.rs
+++ src/bin/8-5-3-0_binding.rs
@@ -2,4 +2,13 @@ fn age() -> u32 {
   15
 }
 
-fn main() {}
+fn main() {
+  println!("その人は何歳なのか？");
+
+  match age() {
+    0 => println!("まだ生まれていない"),
+    n @ 1..=19 => println!("そいつは{}歳で少年", n),
+    n @ 20..=100 => println!("そいつは{}歳で成年", n),
+    n => println!("{}歳を超えているようだ", n),
+  }
+}
