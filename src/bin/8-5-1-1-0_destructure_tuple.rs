b6121cbb30eefd616c6d8eb9f7bad5241cbf6f89
eda3 <gerorin1010@gmail.com>
2021-03-21 11:50:45 +0900

    タプルのデストラクト

--- src/bin/8-5-1-1-0_destructure_tuple.rs
+++ src/bin/8-5-1-1-0_destructure_tuple.rs
@@ -1,1 +1,13 @@
-fn main() {}
+fn main() {
+  let pair = (0, -2);
+
+  println!("pair: {:?}", pair);
+  // -> pair: (0, -2)
+
+  match pair {
+    (0, y) => println!("1つ目が0で、yは{:?}", y),
+    (x, 0) => println!("2つ目が0で、xは{:?}", x),
+    _ => println!("タプルに0が含まれていないパターン"),
+  }
+  // -> 1つ目が0で、yは-2
+}
