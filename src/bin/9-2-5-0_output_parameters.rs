385700e1d7a9d2c7e637156bfb914fabc4f08ed1
eda3 <gerorin1010@gmail.com>
2021-03-26 05:04:41 +0900

    add: create_fn()

--- src/bin/9-2-5-0_output_parameters.rs
+++ src/bin/9-2-5-0_output_parameters.rs
@@ -1,1 +1,26 @@
+// Closures as input parameters are possible, so returning closures as output parameters should also
+// be possible.
+// 入力パラメータとしてのクロージャが可能なので、出力パラメータとしてクロージャを返すことも可能なはずです。
+
+// However, anonymous closure types are, by definition, unknown, so we have to use impl Trait to
+// return them.
+// しかし、無名のクロージャ型は定義上、未知のものなので、それを返すためには impl Trait を使わなければなりません。
+
+// The valid traits for returning a closure are:
+// クロージャを返すのに有効なTraitsは以下の通りです。
+// - Fn
+// - FnMut
+// - FnOnce
+
+// Beyond this, the move keyword must be used, which signals that all captures occur by value
+// その先、moveキーワードを使用する必要があります。これは、すべてのキャプチャーが値によって行われることを示すものです。
+
+// This is required because any captures by reference would be dropped as soon as the function
+// exited, leaving invalid references in the closure.
+// これは、参照によるキャプチャが、関数が終了すると同時に削除され、クロージャに無効な参照が残ってしまうためです。
+
+fn create_fn() -> impl Fn() {
+  let text = "Fn".to_owned();
+  move || println!("これは{}()", text)
+}
 fn main() {}
