e604a04843df67ba093a7fdacf8af9254fb115d2
eda3 <gerorin1010@gmail.com>
2021-03-30 23:37:31 +0900

    ジェネリック型を利用した変数束縛

--- src/bin/14-0_generics.rs
+++ src/bin/14-0_generics.rs
@@ -10,4 +10,16 @@ struct Single(A);
 // 上で定義した`A`を受け取ることもできる。
 struct SingleGen<T>(T);
 
-fn main() {}
+fn main() {
+  // `Single`は具象型で、`A`のみを受け取る。
+  let _s = Single(A);
+
+  // `_char`という名の変数を生成する。これは`SingleGen<char>`という型で、値は`SingleGen('a')`となる。
+  // ここでは、`SingleGen`には明示的な型パラメータが与えられている。
+  let _char: SingleGen<char> = SingleGen('a');
+
+  // `SingleGen`型の変数には明示的に型パラメータを与えなくてもよい。
+  let _t = SingleGen(A); // 上で定義した`A`を使用
+  let _i32 = SingleGen(6); // `i32`を使用
+  let _char = SingleGen('a'); // `char`を使用
+}
