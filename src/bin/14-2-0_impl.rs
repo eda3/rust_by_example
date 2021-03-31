1c635c05f750720a109b3d21647940adb1c37836
eda3 <gerorin1010@gmail.com>
2021-04-01 06:33:30 +0900

    ジェネリック型のメソッド定義

--- src/bin/14-2-0_impl.rs
+++ src/bin/14-2-0_impl.rs
@@ -1,1 +1,13 @@
+struct S; // 具象型`S`
+
+struct GenericVal<T>(T); // ジェネリック型`GenericVal`
+
+// 型パラメータを指定したうえで、GenericValにメソッドを実装
+impl GenericVal<f32> {} // `f32`の場合のメソッド
+
+impl GenericVal<S> {} // 上で定義した`S`への実装
+
+// ジェネリック型のまま扱うには`<T>`が先に来る必要がある。
+impl<T> GenericVal<T> {}
+
 fn main() {}
