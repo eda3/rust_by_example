fb7318d118dffa3ef5fa8307904fc7e1cba04e00
eda3 <gerorin1010@gmail.com>
2021-04-01 22:17:14 +0900

    add: trait HashArea, struct Rectangle, struct Triangle

--- src/bin/14-4-0_bounds.rs
+++ src/bin/14-4-0_bounds.rs
@@ -1,9 +1,26 @@
 use std::fmt::Display;
 
+// print時のマーカー{:?}を実装するtrait
+use std::fmt::Debug;
+
 // `Display`トレイトを実装している`T`を引数として取る
 // `printer`という関数を定義。
 fn printer<T: Display>(t: T) {
   println!("{}", t);
+}
+
+trait HashAreA {
+  fn area(&self) -> f64;
+}
+
+struct Rectangle {
+  length: f64,
+  height: f64,
+}
+
+struct Triangle {
+  length: f64,
+  height: f64,
 }
 
 fn main() {}
