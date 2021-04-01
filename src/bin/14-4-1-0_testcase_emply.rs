b30f0c66cf26ff7774c57d910f4ba2062cf7b96d
eda3 <gerorin1010@gmail.com>
2021-04-01 23:32:15 +0900

    main()

--- src/bin/14-4-1-0_testcase_emply.rs
+++ src/bin/14-4-1-0_testcase_emply.rs
@@ -17,4 +17,24 @@ fn blue<T: Blue>(_: &T) -> &'static str {
   "blue"
 }
 
-fn main() {}
+fn main() {
+  let cardinal = Cardinal;
+  let blue_jay = BlueJay;
+  let _turkey = Turkey;
+
+  // トレイト境界のため、`red`は`blue_jay`に対しては使用できない。
+  // `blue`と`Cardinal`も同様、
+  println!("cardinal: {}", red(&cardinal));
+  // -> cardinal: red
+
+  println!("blue_lay: {}", blue(&blue_jay));
+  // -> blue_lay: blue
+
+  // println!("_turkey: {}", red(&_turkey));
+  // エラー発生する
+  //| fn red<T: Red>(_: &T) -> &'static str {
+  // |           --- required by this bound in `red`
+  // ...
+  // |   println!("_turkey: {}", red(&_turkey));
+  // |                               ^^^^^^^^ the trait `Red` is not implemented for `Turkey`
+}
