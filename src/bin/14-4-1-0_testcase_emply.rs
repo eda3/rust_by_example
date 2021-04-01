03c8b1c254d7d17e60f011e5f88373f339a921d2
eda3 <gerorin1010@gmail.com>
2021-04-01 23:25:18 +0900

    add: red(), blue()

--- src/bin/14-4-1-0_testcase_emply.rs
+++ src/bin/14-4-1-0_testcase_emply.rs
@@ -1,1 +1,20 @@
+struct Cardinal;
+struct BlueJay;
+struct Turkey;
+
+trait Red {}
+trait Blue {}
+
+impl Red for Cardinal {}
+impl Blue for BlueJay {}
+
+// 以下の関数はトレイト境界を設けているが、そのトレイトが空である
+// か否かとは関係がない。
+fn red<T: Red>(_: &T) -> &'static str {
+  "red"
+}
+fn blue<T: Blue>(_: &T) -> &'static str {
+  "blue"
+}
+
 fn main() {}
