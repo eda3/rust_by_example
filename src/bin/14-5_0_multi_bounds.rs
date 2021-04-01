acde540172006f7218039aacd7b2d3646a8d8598
eda3 <gerorin1010@gmail.com>
2021-04-02 07:03:35 +0900

    add: compare_type

--- src/bin/14-5_0_multi_bounds.rs
+++ src/bin/14-5_0_multi_bounds.rs
@@ -4,4 +4,9 @@ fn compare_prints<T: Debug + Display>(t: &T) {
   println!("Debug: {:?}", t);
   println!("Display: {}", t);
 }
+
+fn compare_type<T: Debug, U:Debug>(t: &T, u: &U){
+  println!("t: {:?}", t);
+  println!("u: {:?}", u);
+}
 fn main() {}
