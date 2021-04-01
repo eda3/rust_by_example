ba25d74ef483ab644c769dce9c4527e3c286d03c
eda3 <gerorin1010@gmail.com>
2021-04-02 07:08:19 +0900

    main

--- src/bin/14-5_0_multi_bounds.rs
+++ src/bin/14-5_0_multi_bounds.rs
@@ -5,8 +5,32 @@ fn compare_prints<T: Debug + Display>(t: &T) {
   println!("Display: {}", t);
 }
 
-fn compare_type<T: Debug, U:Debug>(t: &T, u: &U){
+fn compare_type<T: Debug, U: Debug>(t: &T, u: &U) {
   println!("t: {:?}", t);
   println!("u: {:?}", u);
 }
-fn main() {}
+fn main() {
+  let string = "word";
+  let array = [1, 2, 3];
+  let vec = vec![1, 2, 3];
+
+  compare_prints(&string);
+  // -> Debug: "word"
+  // -> Display: word
+
+  // compare_prints(&array);
+  // 上をコメントアウトするとエラー発生
+  //
+  // error[E0277]: `[{integer}; 3]` doesn't implement `std::fmt::Display`
+  // --> src\bin\14-5_0_multi_bounds.rs:21:18
+  //   |
+  //   | fn compare_prints<T: Debug + Display>(t: &T) {
+  //   |                              ------- required by this bound in `compare_prints`
+  //   ...
+  //   |   compare_prints(&array);
+  //   |                  ^^^^^^ `[{integer}; 3]` cannot be formatted with the default formatter
+
+  compare_type(&array, &vec);
+  // -> t: [1, 2, 3]
+  // -> u: [1, 2, 3]
+}
