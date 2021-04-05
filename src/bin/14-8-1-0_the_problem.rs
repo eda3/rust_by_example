7f7db918edad36dce0b9d8ec34ce8077857e8b7c
eda3 <gerorin1010@gmail.com>
2021-04-05 22:46:33 +0900

    add: trait Contains fn contains(), fn first(), fn last()

--- src/bin/14-8-1-0_the_problem.rs
+++ src/bin/14-8-1-0_the_problem.rs
@@ -1,1 +1,13 @@
+struct Container(i32, i32);
+
+// ２つの要素がコンテナ型の中にあることをチェックするトレイト
+// また、最初と最後の値を取得することもできる
+trait Contains<A, B> {
+  fn contains(&self, _: &A, _: &B) -> bool; // `A`と`B`の両方を明示的に要求する
+
+  fn first(&self) -> i32; // `A`、`B`いずれも要求しない
+
+  fn last(&self) -> i32; // `A`、`B`いずれも要求しない
+}
+
 fn main() {}
