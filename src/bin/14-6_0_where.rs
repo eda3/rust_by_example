3f2af7e57cdd19b8bbcb56412ae49e02dcf302be
eda3 <gerorin1010@gmail.com>
2021-04-05 21:00:49 +0900

    add impl PrintInOption

--- src/bin/14-6_0_where.rs
+++ src/bin/14-6_0_where.rs
@@ -4,4 +4,18 @@ trait PrintInOption {
   fn print_in_option(self);
 }
 
+// `where`句を用いない場合、以下と等価な機能を実装するには、
+// `<T: Debug>`という形で表現するか、別の直接的でない方法
+// を使用するかしなくてはならない。
+impl<T> PrintInOption for T
+where
+  Option<T>: Debug,
+{
+  // プリントされるのが`Some(self)`であるため、この関数の
+  // ジェネリック境界として`Option<T>: Debug`を使用したい。
+  fn print_in_option(self) {
+    println!("{:?}", Some(self));
+  }
+}
+
 fn main() {}
