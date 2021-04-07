b29fc11f1dc3a453fce1f11c8617dab6feee9a52
eda3 <gerorin1010@gmail.com>
2021-04-07 21:30:47 +0900

    add impleContains for Conteiner

--- src/bin/14-8-2-types.rs
+++ src/bin/14-8-2-types.rs
@@ -10,7 +10,26 @@ trait Contains {
   fn contains(&self, _: Self::A, _: &Self::B) -> bool;
   fn first(&self) -> i32;
   fn last(&self) -> i32;
+}
 
+impl Contains for Container {
+  // `A`と`B`がどの型であるかを明示。インプットの型（訳注: つまり`Self`の型）
+  // が`Container(i32, i32)`である場合、出力型は`i32`と`i32`となる。
+  type A = i32;
+  type B = i32;
+
+  fn contains(&self, number_1: &i32, number_2: &i32) -> bool {
+    (&self.0 == number_1) && (&self.1 == number_2)
+  }
+  // 1つ目の数を取得
+  fn first(&self) -> i32 {
+    self.0
+  }
+
+  // 最後の数を取得
+  fn last(&self) -> i32 {
+    self.1
+  }
 }
 
 fn main() {}
