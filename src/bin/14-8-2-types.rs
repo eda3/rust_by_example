82f88ca9095e4c696ed63bcff6ff1f0acd467fe9
eda3 <gerorin1010@gmail.com>
2021-04-07 21:26:47 +0900

    add: trait: Contains

--- src/bin/14-8-2-types.rs
+++ src/bin/14-8-2-types.rs
@@ -1,1 +1,16 @@
+struct Container(i32, i32);
+
+// 2つの要素がコンテナ型の中に保持されていることを確認するトレイト。
+// また、最初あるいは最後の要素を取り出すこともできる。
+trait Contains {
+  // メソッドが使用できるジェネリック型を定義
+  type A;
+  type B;
+
+  fn contains(&self, _: Self::A, _: &Self::B) -> bool;
+  fn first(&self) -> i32;
+  fn last(&self) -> i32;
+
+}
+
 fn main() {}
