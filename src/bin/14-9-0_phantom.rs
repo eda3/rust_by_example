6d859f1b709c6c3a0b77a11e5ef71b8c92f272f5
eda3 <gerorin1010@gmail.com>
2021-04-07 22:05:39 +0900

    add: struct PhantomStruct

--- src/bin/14-9-0_phantom.rs
+++ src/bin/14-9-0_phantom.rs
@@ -4,4 +4,11 @@ use std::marker::PhantomData;
 #[derive(PartialEq)] // 比較演算子(`==`)での比較を可能にする。
 struct PhantomTuple<A, B>(A, PhantomData<B>);
 
+// 同様に構造体を定義
+#[derive(PartialEq)] // 比較演算子での比較を可能にする。
+struct PhantomStruct<A, B> {
+  first: A,
+  phantom: PhantomData<B>,
+}
+
 fn main() {}
