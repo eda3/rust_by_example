588c0a4816c63666a3e59e576e04401c9749ce4e
eda3 <gerorin1010@gmail.com>
2021-04-18 11:53:54 +0900

    add: struct Book

--- src/bin/15-3-1-0_mut.rs
+++ src/bin/15-3-1-0_mut.rs
@@ -1,1 +1,10 @@
+#[allow(dead_code)]
+#[derive(Clone, Copy)]
+struct Book {
+  // &'static str はread-onlyメモリ上の文字列への参照
+  author: &'static str,
+  title: &'static str,
+  year: u32,
+}
+
 fn main() {}
