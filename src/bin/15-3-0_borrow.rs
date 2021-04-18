bf64feae5b284b35379ee90b819eaaaf2f55cb2b
eda3 <gerorin1010@gmail.com>
2021-04-18 11:00:09 +0900

    Box化された整数の作成と借用

--- src/bin/15-3-0_borrow.rs
+++ src/bin/15-3-0_borrow.rs
@@ -7,4 +7,16 @@ fn borrow_i32(borrowed_i32: &i32) {
   println!("i32型の値: {}", borrowed_i32);
 }
 
-fn main() {}
+fn main() {
+  // Box化された整数を作成
+  let boxed_i32 = Box::new(5i32);
+  let stacked_i32 = 6i32;
+
+  // Boxの中身を借用。所有権を奪うわけではないため、
+  // 直後にもう一度借用できる
+  borrow_i32(&boxed_i32);
+  // -> i32型の値: 5
+
+  borrow_i32(&stacked_i32);
+  // -> i32型の値: 6
+}
