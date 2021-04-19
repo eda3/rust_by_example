578cf9ce5ffb6696e61e7d3e6248eeb510e0bb70
eda3 <gerorin1010@gmail.com>
2021-04-19 19:40:30 +0900

    add: Pointの各値を表示

--- src/bin/15-3-2_alias.rs
+++ src/bin/15-3-2_alias.rs
@@ -4,4 +4,16 @@ struct Point {
   z: i32,
 }
 
-fn main() {}
+fn main() {
+  let mut point = Point { x: 0, y: 0, z: 0 };
+
+  let borrowed_point = &point;
+  let another_borrow = &point;
+
+  // データはもともとの持ち主と参照の両方からアクセスすることが出来ます。
+  println!(
+    "Pointの各値:({}, {}, {})",
+    borrowed_point.x, another_borrow.y, point.z
+  );
+  // -> Pointの各値:(0, 0, 0)
+}
