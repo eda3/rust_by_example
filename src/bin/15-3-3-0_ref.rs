bac185d141b4355305fc6d4a289df89ebbc197f0
eda3 <gerorin1010@gmail.com>
2021-04-27 19:17:49 +0900

    左辺にrefをつけたパターンと&をつけたパターンは等価

--- src/bin/15-3-3-0_ref.rs
+++ src/bin/15-3-3-0_ref.rs
@@ -4,4 +4,12 @@ struct Point {
   y: i32,
 }
 
-fn main() {}
+fn main() {
+  let c = 'Q';
+
+  // 左辺に ref をつけることによる借用と & をつけることによる借用は等価
+  let ref ref_c1 = c;
+  let ref_c2 = &c;
+
+  println!("*ref_c1 == *ref_c2: {}", *ref_c1 == *ref_c2);
+}
