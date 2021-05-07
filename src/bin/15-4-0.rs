be78c7763593bf9e2797464cdbaf92e4c9223535
eda3 <gerorin1010@gmail.com>
2021-05-08 04:54:54 +0900

    cf

--- src/bin/15-4-0.rs
+++ src/bin/15-4-0.rs
@@ -4,18 +4,20 @@
 // 重なりません
 fn main() {
   let i = 3; // Lifetime for `i` starts. ────────────────┐
-  //                                                     │
-  { //                                                   │
+             //                                                     │
+  {
+    //                                                   │
     let borrow1 = &i; // `borrow1` lifetime starts. ──┐│
-    //                                                ││
+                      //                                                ││
     println!("borrow1: {}", borrow1); //              ││
   } // `borrow1 ends. ──────────────────────────────────┘│
-  //                                                     │
-  //                                                     │
-  { //                                                   │
+    //                                                     │
+    //                                                     │
+  {
+    //                                                   │
     let borrow2 = &i; // `borrow2` lifetime starts. ──┐│
-    //                                                ││
+                      //                                                ││
     println!("borrow2: {}", borrow2); //              ││
   } // `borrow2` ends. ─────────────────────────────────┘│
-  //                                                     │
-}   // Lifetime ends. ─────────────────────────────────────┘
+    //                                                     │
+} // Lifetime ends. ─────────────────────────────────────┘
