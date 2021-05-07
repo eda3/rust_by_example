2d0162533cfa561a12cb99c683d30aa6d63fce45
eda3 <gerorin1010@gmail.com>
2021-05-08 04:48:12 +0900

    lifetime

---
+++ src/bin/15-4-0.rs
@@ -0,0 +1,21 @@
+// 以下では、変数の作成から破棄までのライフタイムを線で示しています
+// iは最長のライフタイムを持ち、そのスコープはborrow1およびborrow2
+// のスコープを完全に包含します。borrow1とborrow2の存続期間は一切
+// 重なりません
+fn main() {
+  let i = 3; // Lifetime for `i` starts. ────────────────┐
+  //                                                     │
+  { //                                                   │
+    let borrow1 = &i; // `borrow1` lifetime starts. ──┐│
+    //                                                ││
+    println!("borrow1: {}", borrow1); //              ││
+  } // `borrow1 ends. ──────────────────────────────────┘│
+  //                                                     │
+  //                                                     │
+  { //                                                   │
+    let borrow2 = &i; // `borrow2` lifetime starts. ──┐│
+    //                                                ││
+    println!("borrow2: {}", borrow2); //              ││
+  } // `borrow2` ends. ─────────────────────────────────┘│
+  //                                                     │
+}   // Lifetime ends. ─────────────────────────────────────┘
