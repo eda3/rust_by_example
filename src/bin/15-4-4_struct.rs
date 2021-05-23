c0542e1291289fcb80284957bf7d9198350e19a6
eda3 <gerorin1010@gmail.com>
2021-05-23 10:55:58 +0900

    add: struct Borrowed

--- src/bin/15-4-4_struct.rs
+++ src/bin/15-4-4_struct.rs
@@ -1,1 +1,7 @@
+// `i32`. The reference to `i32` must outlive `Borrowed`.
+// `i32`への参照をメンバに持つ`Borrowed`型。
+// 参照は`Borrowed`自体よりも長生きでなくてはならない。
+#[derive(Debug)]
+struct Borrowed<'a>(&'a i32);
+
 fn main() {}
