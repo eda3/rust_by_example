b8ce4d48c123271843180d57c13f74889ef62f2d
eda3 <gerorin1010@gmail.com>
2021-05-23 13:51:43 +0900

    main()

--- src/bin/15-4-4_struct.rs
+++ src/bin/15-4-4_struct.rs
@@ -12,9 +12,27 @@ struct NamedBorrowed<'a> {
 }
 
 // i32、あるいはi32への参照のいずれかとなる列挙型
+#[derive(Debug)]
 enum Either<'a> {
   Num(i32),
   Ref(&'a i32),
 }
 
-fn main() {}
+fn main() {
+  let x = 18;
+  let y = 15;
+
+  let single = Borrowed(&x);
+  let double = NamedBorrowed { x: &x, y: &y };
+  let reference = Either::Ref(&x);
+  let number = Either::Num(y);
+
+  println!("x is borrowed in {:?}", single);
+  // -> x is borrowed in Borrowed(18)
+  println!("x an y are borrowed in {:?}", double);
+  // -> x an y are borrowed in NamedBorrowed { x: 18, y: 15 }
+  println!("x is borrowed in {:?}", reference);
+  // -> x is borrowed in Ref(18)
+  println!("y is *not* borrowed in {:?}", number);
+  // -> y is *not* borrowed in Num(15)
+}
