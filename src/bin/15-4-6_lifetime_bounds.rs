bf85e50421f8962dc275d65ac7e8d73db96a9e77
eda3 <gerorin1010@gmail.com>
2021-05-29 08:59:45 +0900

    add: fn print()

--- src/bin/15-4-6_lifetime_bounds.rs
+++ src/bin/15-4-6_lifetime_bounds.rs
@@ -6,4 +6,11 @@ struct Ref<'a, T: 'a>(&'a T);
 // Tの値 *に対する参照* は必ず'aよりも長生きでなくてはならない。
 // さらに、Refのライフタイムは'aを超えてはならない。
 
+//  Debugトレイトを利用してprintを行うジェネリック関数
+fn print<T>(t: T)
+where
+  T: Debug,
+{
+  println!("print(): t is {:?}", t);
+}
 fn main() {}
