6a6ce944bf302d0525297dbb34073341a008778a
eda3 <gerorin1010@gmail.com>
2021-07-08 19:00:08 +0900

    main

--- src/bin/15-4-7_coercion.rs
+++ src/bin/15-4-7_coercion.rs
@@ -4,4 +4,22 @@ fn multiply<'a>(first: &'a i32, second: &'a i32) -> i32 {
   first * second
 }
 
-fn main() {}
+// `<'a: 'b, 'b>`は「ライフタイム`'a`は最低でも`'b`と同じ長さ」と読める。
+// ここでは、`&'a i32`をとり、`&'b i32`に圧縮して返す。
+fn choose_first<'a: 'b, 'b>(first: &'a i32, _: &'b i32) -> &'b i32 {
+  first
+}
+
+fn main() {
+  let first = 2; // 長いライフタイム
+
+  {
+    let second = 3; // 短いライフタイム
+
+    println!("The product is {}", multiply(&first, &second));
+    // -> The product is 6
+
+    println!("{} is the first", choose_first(&first, &second));
+    // -> 2 is the first
+  }
+}
